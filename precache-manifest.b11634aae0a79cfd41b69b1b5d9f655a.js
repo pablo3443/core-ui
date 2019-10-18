3290383293719eb69dbba5af36d2616ae9b5d704
Travis-CI <comindware-awesome-b@comindware.com>
2019-10-18 15:13:13 +0000

    Auto-deploy to Github Pages

---
+++ precache-manifest.b11634aae0a79cfd41b69b1b5d9f655a.js
@@ -0,0 +1,44 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d46646b855212ed4d2",
+    "url": "app.js"
+  },
+  {
+    "revision": "657a469469a2fc38a2901c68a3d56512",
+    "url": "fonts/fa-solid-900.woff"
+  },
+  {
+    "revision": "2cd2be177470d5096992572176bbe76e",
+    "url": "fonts/fa-solid-900.woff2"
+  },
+  {
+    "url": "fonts/fdc155d57b7351ef85b3028ea3cfc048.svg"
+  },
+  {
+    "url": "icon_128x128.714d63aeda32d131b01d0ef137fd68a1.png"
+  },
+  {
+    "url": "icon_144x144.1a171c39c07bf6b29d652e00a46adc2d.png"
+  },
+  {
+    "url": "icon_152x152.b2666e72872ff41915d0a403f7d64ecf.png"
+  },
+  {
+    "url": "icon_192x192.3420fe48c7b98b3e48be6ef2d8058cd0.png"
+  },
+  {
+    "url": "icon_384x384.7b318db198bec11c4586efb16efe2928.png"
+  },
+  {
+    "url": "icon_512x512.fcff4271d5ae3728a9de51fcd51f1bc8.png"
+  },
+  {
+    "url": "icon_72x72.120a434576809a7246b0e3370eef5f3f.png"
+  },
+  {
+    "url": "icon_96x96.7aff45e523c76f5d83459fe6ebd25fe6.png"
+  },
+  {
+    "url": "manifest.734dbca253a12f888fe913f16e11fb50.json"
+  }
+]);
